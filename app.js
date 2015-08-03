67391677fc89a1e6722eef892c599f52d5317f88
Jenan Mannette <jenanmannette@gmail.com>
2015-08-02 19:29:55 -0600

    screencast code

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/index');
+    res.redirect('/index?');
 });
 
 
